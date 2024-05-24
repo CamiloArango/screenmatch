d27976e30f03772efc36c1ff64d5a63350fbf591
Camilo Arango <76237032+CamiloArango@users.noreply.github.com>
2024-05-24 11:12:34 -0500

    upload

---
+++ src/Principal.java
@@ -0,0 +1,18 @@
+import com.aluracursos.screenmatch.modelos.Pelicula;
+
+public class Principal {
+    public static void main(String[] args) {
+        Pelicula miPelicula = new Pelicula();
+        miPelicula.setNombre("The Perks of Being a Wallflower");
+        miPelicula.setFechaDeLanzamiento(2021);
+        miPelicula.setDuracionEnMinutos(120);
+        miPelicula.setIncluidoEnElPlan(true);
+
+        miPelicula.muestraFichaTecnica();
+        miPelicula.evalua(10);
+        miPelicula.evalua(10);
+        System.out.println(miPelicula.getTotalDeLasEvalaciones());
+        System.out.println(miPelicula.calculaMedia());
+
+    }
+}
